d555bd0faa8ef7160f50b68081fff0e261eaf5ab
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-01-22 02:28:06 +0100

    Check if the game is supported, if so load the gui

---
+++ handler.lua
@@ -0,0 +1,7 @@
+local PrisonLife = 155615604
+
+if game.PlaceId == PrisonLife then
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/prison_life.lua')))
+else
+ game.Players.LocalPlayer:Kick('[Syntaxed]: Game not supported')
+end
