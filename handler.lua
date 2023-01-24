737b9768dc1b28cb11300910c05e6186fff27cfe
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-01-24 12:45:22 +0100

    Fixed unable to load GUI

--- handler.lua
+++ handler.lua
@@ -1,6 +1,4 @@
-local PrisonLife = 155615604
-
-if game.PlaceId == PrisonLife then
+if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/prison_life.lua')))
 else
  game.Players.LocalPlayer:Kick('[Syntaxed]: Game not supported')
