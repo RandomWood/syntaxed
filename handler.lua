37a2523fdca0740da251f26bff033345ea8b2e1a
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-01-28 23:24:35 +0100

    Brought back not supported

--- handler.lua
+++ handler.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/prison_life.lua', true))()
+else
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/NotSupported.lua', true))()
 end
