676fbc7f93fc7e5fcc4d1ff1d3fd0c2ac5e962af
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-01-28 23:16:47 +0100

    Added true and got rid of debugger

--- handler.lua
+++ handler.lua
@@ -1,4 +1,3 @@
 if game.PlaceId == 155615604 then
- print('Syntaxed me meow')
- --[[loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/prison_life.lua'))()--]]
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/prison_life.lua', true))()
 end
