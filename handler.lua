e55ba941044d9b1ffecc6667022e2f52f2baeeea
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-01-28 22:27:00 +0100

    Got rid of unsupported loader

--- handler.lua
+++ handler.lua
@@ -1,5 +1,2 @@
 if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/prison_life.lua'))()
-else
- loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/NotSupported.lua'))()
-end
