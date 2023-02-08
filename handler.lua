08235c4c1bb9a550e32bcfab351fa9747d67eac3
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-02-08 14:51:56 +0100

    Loading with .Lua extension for +1BES

--- handler.lua
+++ handler.lua
@@ -2,7 +2,7 @@ if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/Prison%20Life.lua', true))()
 end
 elseif game.PlaceId == 11800876530 then
- loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second', true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second.lua', true))()
 end
 else
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/NotSupported.lua', true))()
