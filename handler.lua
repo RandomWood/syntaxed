c2b19dac8216685f68bf9fda5d5d0e68a0493cf3
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-02-08 14:58:44 +0100

    Got rid of 2 end functions

--- handler.lua
+++ handler.lua
@@ -1,9 +1,7 @@
 if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/Prison%20Life.lua', true))()
-end
 elseif game.PlaceId == 11800876530 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second.lua', true))()
-end
 elseif game.PlaceId !== 155615604 or game.PlaceId !== 11800876530 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/NotSupported.lua', true))()
 end
