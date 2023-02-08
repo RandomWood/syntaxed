78f1e527a6d5e4463ce1bb7868c51c600e639450
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-02-08 14:56:56 +0100

    Fixed not supported

--- handler.lua
+++ handler.lua
@@ -4,6 +4,6 @@ end
 elseif game.PlaceId == 11800876530 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second.lua', true))()
 end
-else
+elseif game.PlaceId !== 155615604 or game.PlaceId !== 11800876530 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/NotSupported.lua', true))()
 end
