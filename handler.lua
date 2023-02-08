88a00f144dbdf4a2b4d88fb78c4a2b8040b4572f
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-02-08 15:01:50 +0100

    Got rid of not supported GUU

--- handler.lua
+++ handler.lua
@@ -2,6 +2,4 @@ if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/Prison%20Life.lua', true))()
 elseif game.PlaceId == 11800876530 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second.lua', true))()
-elseif game.PlaceId !== 155615604 or game.PlaceId !== 11800876530 then
- loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/NotSupported.lua', true))()
 end
