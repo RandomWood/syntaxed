a3cfa612301ae4062776f0b22ee132058b5fbf82
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-02-08 14:49:02 +0100

    Fixed +1BES loading Prison Life

--- handler.lua
+++ handler.lua
@@ -1,7 +1,9 @@
 if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/Prison%20Life.lua', true))()
+end
 elseif game.PlaceId == 11800876530 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second', true))()
+end
 else
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/NotSupported.lua', true))()
 end
