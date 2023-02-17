3614047a113856e4c02e722a1850e7cd4ff488fb
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-02-17 13:20:52 +0100

    Shorter wait & not supported notification

--- handler.lua
+++ handler.lua
@@ -3,13 +3,17 @@ local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
 	Title = "Syntaxed";
 	Text = "Loading the GUI...";
-	Duration = 5;
+	Duration = 4;
 })
-wait(5)
+wait(3)
 if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/Prison%20Life.lua', true))()
 elseif game.PlaceId == 11800876530 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second.lua', true))()
 else
-print('test')
+CoreGui:SetCore("SendNotification", {
+	Title = "Syntaxed";
+	Text = "Game not supported";
+	Duration = 4;
+})
 end
