3efc060a9f71e2022f7c3841cbc02815a3cbd488
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-02-17 13:54:39 +0100

    Discontinued +1 Blocks Every Second

--- handler.lua
+++ handler.lua
@@ -9,7 +9,12 @@ wait(2)
 if game.PlaceId == 155615604 then
  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/Prison%20Life.lua', true))()
 elseif game.PlaceId == 11800876530 then
- loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second.lua', true))()
+   CoreGui:SetCore("SendNotification", {
+	Title = "Syntaxed";
+	Text = "Please note that Syntaxed for this game has been discontinued meaning no further updates will be released";
+	Duration = 4;
+})
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/DatEmage/syntaxed/main/Games/%2B1%20Blocks%20Every%20Second.lua', true))()
 else
 CoreGui:SetCore("SendNotification", {
 	Title = "Syntaxed";
